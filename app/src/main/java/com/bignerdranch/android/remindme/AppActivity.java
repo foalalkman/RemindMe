19bd19657f0657ef50758e66c0843acc40d7ead0
Annika Svedin <annika.svedin@gmail.com>
2017-08-10 12:51:31 +0200

    working with activities

--- app/src/main/java/com/bignerdranch/android/remindme/AppActivity.java
+++ app/src/main/java/com/bignerdranch/android/remindme/AppActivity.java
@@ -1,27 +1,39 @@
 package com.bignerdranch.android.remindme;
 
-        import android.content.pm.PackageManager;
-        import android.support.annotation.NonNull;
-        import android.support.v4.app.ActivityCompat;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 
-        import android.support.v4.app.Fragment;
-        import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 
 
-        import android.support.v4.app.FragmentTransaction;
-        import android.support.v4.content.ContextCompat;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.support.v7.widget.Toolbar;
-        import android.view.Menu;
-        import android.view.MenuItem;
-        import android.widget.FrameLayout;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
-        import java.util.List;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+import java.util.List;
 
 public class AppActivity extends AppCompatActivity {
 
-    private FrameLayout fragmentFrame;
+    private static final int PLACE_PICKER_REQUEST = 199;
+    private int status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +43,19 @@ public class AppActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
-        fragmentFrame = (FrameLayout) findViewById(R.id.content_fragment);
+//        checkGooglePlayServicesAvailability();
+
+        FrameLayout fragmentFrame = (FrameLayout) findViewById(R.id.content_fragment);
+    }
+
+    private void checkGooglePlayServicesAvailability() {
+        status = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+        if (status != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(status)) {
+                GooglePlayServicesUtil.getErrorDialog(status, AppActivity.this,
+                        100).show();
+            }
+        }
     }
 
     @Override
@@ -47,6 +71,29 @@ public class AppActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        if (id == R.id.new_reminder) {
+            status = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+            if (status == ConnectionResult.SUCCESS) {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+                try {
+                    startActivityForResult(builder.build(AppActivity.this), PLACE_PICKER_REQUEST);
+
+                } catch (GooglePlayServicesRepairableException e) {
+                    Toast.makeText(this, "exception", Toast.LENGTH_LONG).show();
+
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    Toast.makeText(this, "exception", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            } else {
+                Toast.makeText(this, "NOT", Toast.LENGTH_LONG).show();
+            }
+
+            return true;
+        }
+
         if (id == R.id.list_reminders) {
             Fragment fragment = new MyListFragment();
 
@@ -57,17 +104,20 @@ public class AppActivity extends AppCompatActivity {
             return true;
         }
 
-        if (id == R.id.new_reminder) {
-            Fragment fragment = new MyMapFragment();
-
-            FragmentManager fm = getSupportFragmentManager();
-            FragmentTransaction transaction = fm.beginTransaction();
-            transaction.replace(R.id.content_fragment, fragment);
-            transaction.commit();
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == PLACE_PICKER_REQUEST) {
+
+            if (resultCode == RESULT_OK) {
+
+                Place place = PlacePicker.getPlace(data, this);
+                String toastMsg = String.format("Place: %s", place.getName());
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/bignerdranch/android/remindme/NewReminderFragment.java
@@ -0,0 +1,64 @@
+package com.bignerdranch.android.remindme;
+
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+        import com.google.android.gms.common.ConnectionResult;
+        import com.google.android.gms.common.GoogleApiAvailability;
+        import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+        import com.google.android.gms.common.GooglePlayServicesRepairableException;
+        import com.google.android.gms.location.places.ui.PlacePicker;
+
+/**
+ * Created by annika on 2017-08-09.
+ */
+
+public class NewReminderFragment extends android.support.v4.app.Fragment {
+
+    View view;
+    private TextView tempView;
+    private static final int PLACE_PICKER_REQUEST = 199;
+    private int status;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        view = inflater.inflate(R.layout.new_reminder_fragment_view, container, false);
+        tempView = (TextView) view.findViewById(R.id.temp_text_view);
+
+
+        status = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(getActivity());
+        if (status == ConnectionResult.SUCCESS) {
+            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+            try {
+                startActivityForResult(builder.build(getActivity()), PLACE_PICKER_REQUEST);
+
+            } catch (GooglePlayServicesRepairableException e) {
+                Toast.makeText(getActivity(), "exception", Toast.LENGTH_LONG).show();
+
+                e.printStackTrace();
+            } catch (GooglePlayServicesNotAvailableException e) {
+                Toast.makeText(getActivity(), "exception", Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(getActivity(), "NOT", Toast.LENGTH_LONG).show();
+        }
+
+        return view;
+    }
+
+
+
+}
